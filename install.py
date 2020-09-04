be611266b158646a90d47ca7ab7d969591fad22b
Felix Schürmeyer <fschuermeyer@gmail.com>
2020-09-04 16:41:04 +0200

    add Install File

---
+++ install.py
@@ -0,0 +1,21 @@
+import os
+from shutil import copyfile
+
+installPath = "/usr/local/bin"
+possiblePaths = os.environ['PATH'].split(':')
+commmandName = "gcp"
+fileName = "generate-password.py"
+
+if(installPath in possiblePaths):
+    copyfile(fileName, installPath + "/" + commmandName)
+
+    os.chdir(installPath)
+
+    os.system('chmod +x ' + commmandName)
+
+    print("Install Command (" + commmandName + ")")
+else:
+    print("Command ("+ commmandName + ") - not Installed")
+
+
+    
